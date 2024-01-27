0ed3f67a292d10e7444ca847e0ac8abb87bd180c
SAC6969 <sachinvermaa1234@gmail.com>
2024-01-27 12:53:37 +0530

    vercel deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // mongoose.connect('mongodb://localhost:27017/paytm')
-const connectionString = `mongodb+srv://sachin:sac6969@cluster0.wjmetkt.mongodb.net/paytm`;
+const connectionString = process.env.MONGODB;
 
 mongoose.connect(connectionString)
     .then(() => console.log('Connected!'));
